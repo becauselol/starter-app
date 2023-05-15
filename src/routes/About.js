31fe9cca134eec2798efba0913c95cc6e9cd9bfb
becauselol <63877847+becauselol@users.noreply.github.com>
2023-05-15 14:00:11 +0800

    add routing

---
+++ src/routes/About.js
@@ -0,0 +1,11 @@
+function About() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> About </h1>
+      </header>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/routes/Careers.js
@@ -0,0 +1,11 @@
+function Careers() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Careers </h1>
+      </header>
+    </div>
+  );
+}
+
+export default Careers;
